606871147168910c4121aea6699eacad5945da66
AbdulMoiz8994 <moiza8994@gmail.com>
2021-07-09 16:13:05 +0500

    add the stop and and Reset Functionality

--- src/Components/Timer/Timer.tsx
+++ src/Components/Timer/Timer.tsx
@@ -10,13 +10,21 @@ export const Timer = () => {
    const [inter, setInterv]=useState<any>()
 
 function Run(){
+    // start()
+    if(status === 0){   
   setInterv(
       setInterval(() =>{
           start()
       },1000)
   )
   setStatus(1) 
+  console.log(status);
+  
 }
+
+}
+console.log(status);
+
 function start(){
     if(timer.seconds === 60){
         ++timer.minutes
@@ -26,7 +34,24 @@ function start(){
         ++timer.hour
         timer.minutes=0
     }
+    // we can add th else{} or retrun as well
+    
  return setTimer({hour: timer.hour, minutes: timer.minutes, seconds: ++timer.seconds})
+}
+
+
+function Stop(){
+    if(status !== 0){   
+    clearInterval(inter)
+    setStatus(0)
+}
+}
+// console.log(inter);
+
+function Reset(){
+    setTimer({hour: 0 , minutes: 0, seconds: 0})
+    setStatus(0)
+    clearInterval(inter)
 }
 
     return (
@@ -35,6 +60,8 @@ function start(){
             <span>{timer.minutes < 10 ? `0${timer.minutes}` : timer.minutes}</span>
             <span>{timer.seconds < 10 ? `0${timer.seconds}` : timer.seconds}</span>
             <button onClick={Run}>Start</button>
+            <button onClick={Stop}>Stop</button>
+            <button onClick={Reset}>Reset</button>
         </div>
     )
 }

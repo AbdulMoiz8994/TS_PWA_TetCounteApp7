d7af3fdd43e4be9c6a8f576802978741f1e0954c
AbdulMoiz8994 <moiza8994@gmail.com>
2021-07-12 14:34:33 +0500

    add the Dots in

--- src/Components/TimerNumber/TimerNumber.tsx
+++ src/Components/TimerNumber/TimerNumber.tsx
@@ -5,8 +5,8 @@ import './TimerNumber.css'
 export const TimerNumber: React.FC<TimerNumberType> = ({timer:{hour,minutes,seconds}}) => {
     return (
         <div className="time-display">
-            <span>{hour < 10 ? `0${hour}` : hour}</span>
-            <span>{minutes < 10 ? `0${minutes}` : minutes}</span>
+            <span>{hour < 10 ? `0${hour}` : hour}:</span>
+            <span>{minutes < 10 ? `0${minutes}` : minutes}:</span>
             <span>{seconds < 10 ? `0${seconds}` : seconds}</span>
 
         </div>
